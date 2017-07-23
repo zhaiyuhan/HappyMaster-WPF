299b90cd3fb9c83683efdeae34ecfc8b52ec92f8
翟长腿 <zhaiyuhanx@hotmail.com>
2017-07-23 19:22:57 +0800

    want to add Commander

---
+++ Foundation/HandleCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyMaster.Foundation
+{
+    partial class HandleCommand
+    {
+        enum Commands
+        {
+            ERRO_COMMAND,
+            PLAY_NO_URL,
+            PLAY_WITH_URL
+        };
+        Commands _handle(string _input)
+        {
+            Commands _thiscommand = Commands.ERRO_COMMAND;
+            string removeblank = _input.Trim().ToLower();
+            string[] split = removeblank.Split(new Char[] { '(' });
+            return _thiscommand;
+        }
+    }
+}
